faca39873e4b8e3b8d81ab4e163fa56bf9b1c900
dvsgithub <47431465+dvsgithub@users.noreply.github.com>
2020-11-29 22:20:59 +0200

    Add PostConstruct method

--- src/main/java/com/ma/PrinterGreeting.java
+++ src/main/java/com/ma/PrinterGreeting.java
@@ -3,14 +3,23 @@ package com.ma;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class PrinterGreeting implements Printer {
 
     @Autowired
     private GreetingService greetingService;
 
+    private String message;
+
+    @PostConstruct
+    public void init() {
+        message = greetingService.sayGreeting();
+    }
+
     @Override
     public void printMessage() {
-        System.out.println("Print on console: "+ greetingService.sayGreeting());
+        System.out.println("Print on console: " + message);
     }
 }
